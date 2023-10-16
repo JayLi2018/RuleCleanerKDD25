27ff7c8564c74155f4fbe5519e95af2e8ce67510
jayli2018 <lchenjie0302@gmail.com>
2023-10-16 00:16:46 -0500

    anonymized first commit

--- rbbm_src/dc_src/create_tables.sql
+++
@@ -1,3 +0,0 @@
-create table adult(age int,workclass text,fnlwgt int,education text,Education_num int,Marital_status text,occupation text,relationship text,race text,sex text,capital_gain float,capital_loss float,Hours_per_week float,Native_country text,class text)
-
-create table airport(id text,ident text,type text,name text,latitude_deg float,longitude_deg float,elevation_ft int,continent text,iso_country text,iso_region text,municipality text,scheduled_service text,gps_code text,iata_code text,local_code text,home_link text,wikipedia_link text,keywords text)
--- rbbm_src/labelling_func_src/src/test_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python LFRepair.py -U 20 -t 0.5 -e test_609 -r information_gain -f intro -D 0
-python LFRepair.py -U 20 -t 0.5 -e test_609 -r brute_force -f intro -D 0
-python LFRepair.py -U 20 -t 0.5 -e test_609 -r naive -f intro -D 0
-python LFRepair.py -U 20 -t 0.5 -e test_609 -r information_gain -f intro -D 0.5
-python LFRepair.py -U 20 -t 0.5 -e test_609 -r brute_force -f intro -D 0.5
-python LFRepair.py -U 20 -t 0.5 -e test_609 -r naive -f intro -D 0.5
